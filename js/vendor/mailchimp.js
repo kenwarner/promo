3e76e5032052374d4f380f04841a23e70df0d57b
Ken Warner <kjwarner@gmail.com>
2014-03-03 10:51:03 -0600

    update mailchimp form

--- js/vendor/mailchimp.js
+++ js/vendor/mailchimp.js
@@ -62,7 +62,7 @@ function mce_init_form(){
       var options = { errorClass: 'mce_inline_error', errorElement: 'div', onkeyup: function(){}, onfocusout:function(){}, onblur:function(){}  };
       var mce_validator = $("#mc-embedded-subscribe-form").validate(options);
       $("#mc-embedded-subscribe-form").unbind('submit');//remove the validator so we can get into beforeSubmit on the ajaxform, which then calls the validator
-      options = { url: 'http://vidcode.us3.list-manage.com/subscribe/post-json?u=8f7a2f33de0c433f83777b25b&id=b6d9b3556d&c=?', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
+      options = { url: 'http://vidcode.us3.list-manage.com/subscribe/post-json?u=8f7a2f33de0c433f83777b25b&id=45734be307&c=?', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
                     beforeSubmit: function(){
                         $('#mce_tmp_error_msg').remove();
                         $('.datefield','#mc_embed_signup').each(
