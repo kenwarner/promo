213a73b8bf22d9f7df79199630ec959f2d31cecc
Ken Warner <kjwarner@gmail.com>
2014-03-03 10:38:34 -0600

    make signup form errors more gooder

--- js/vendor/mailchimp.js
+++ js/vendor/mailchimp.js
@@ -22,7 +22,7 @@ var err_style = '';
 try{
     err_style = mc_custom_error_style;
 } catch(e){
-    err_style = '#mc_embed_signup input.mce_inline_error{border-color:#6B0505;} #mc_embed_signup div.mce_inline_error{margin: 0 0 1em 0; padding: 5px 10px; background-color:#6B0505; font-weight: bold; z-index: 1; color:#fff;}';
+    err_style = '#mc_embed_signup input.mce_inline_error{border-color:#6B0505;} #mc_embed_signup div.mce_inline_error{margin: 0 0 1em 0; padding: 5px 10px; font-weight: bold; z-index: 1; }';
 }
 var head= document.getElementsByTagName('head')[0];
 var style= document.createElement('style');
